f20539a2212a6376b17eb5889837cbd8fb434d60
nicolonso <nalfonso@student.42lisboa.com>
2025-05-12 22:06:59 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,32 +6,84 @@
 /*   By: nalfonso <nalfonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 20:30:32 by nalfonso          #+#    #+#             */
-/*   Updated: 2025/05/10 17:15:57 by nalfonso         ###   ########.fr       */
+/*   Updated: 2025/05/12 22:05:54 by nalfonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ftprintf.h"
 
+int ft_putcharpf(char c)
+{
+	return(write (1, &c, 1));
+}
+int ft_putstrpf(char *str)
+{
+	int count;
+
+	count = 0;
+	if (!str)
+		str = "(null)";
+	count += ft_putcharpf(*str++);
+	return (count);
+}
+
+static int ft_putbases(number nbr,int len, char *base)
+{
+	int count;
+
+	count = 0;
+	if (nbr >= (unsigned long)len)
+	{
+		count += ft_putbases(nbr / len, len ,base);
+		count += ft_putbases(nbr % len, len ,base);
+	}
+	else
+		count += ft_putcharpf((char *)nbr + *base);
+	return (count);
+}
+static int ft_baseten(long nbr)
+{
+	int count;
+
+	count = 0;
+	if (nbr < 0)
+	{
+		count += ft_putcharpf('-');
+		nbr = -nbr;
+	}
+	count += ft_putbases(nbr, 10, HEXADECIMAL);
+	return (count);
+}
+
 int ft_format(va_list args, char *format, size_t *count)
 {
-	if (args == 'c')
-		return (ft_putchar_pf(args, count));
-	else if (args == 's')
-		return (ft_putstrpf(args, count));
-	else if (args == 'p')
-		return (ft_putnbrhexa(args,count));
-	else if (args == 'd')
-		return (ft_putnbrbase(args,count));
-	else if (args == 'i')
-		return (ft_putnbrbase(args,count));
-	else if (args == 'u')
-		return (ft_putnbrbase(args,count));
-	else if (args == 'x')
-		return (ft_putnbrhexa(args,count));
-	else if (args == 'X')
-		return (ft_putnbrhexa(args,count));
-	else if (args == '%')
+	if (*format == 'c')
+		return (ft_putcharpf(va_arg(args, char)));
+	else if (*format == 's')
+		return (ft_putstrpf(va_arg(args, char *)));
+	else if (*format == 'p')
+	{
+		if (!format)
+		{
+			format = "(nil)";
+			ft_putcharpf(*format++);
+		}
+		ft_putstrpf("0x");
+		return (ft_putbases(va_arg(args,void *) , 16, HEXADECIMAL));
+	}
+	else if (*format == 'd')
+		return (ft_baseten(va_arg(args, int)));
+	else if (*format == 'i')
+		return (ft_baseten(va_arg(args, int)));
+	else if (*format == 'u')
+		return (ft_baseten(va_arg(args, unsigned int)));
+	else if (*format == 'x')
+		return (ft_putbases(va_arg(args,unsigned int) , 16, HEXADECIMAL));
+	else if (*format == 'X')
+		return (ft_putbases(va_arg(args,unsigned int) , 16, CHEXADECIMAL));
+	else if (*format == '%')
 		return (ft_putcharpf('%'));
+	return (0);
 }
 
 int ft_printf(const char *format, ...)
@@ -46,10 +98,18 @@ int ft_printf(const char *format, ...)
 		if(*format == '%')
 		{
 			format++;
-			ft_format(args, (char *)format, &count);
+			count += ft_format(args, (char *)format, &count);
 		}
 		else
-			ft_putchar_pf(format, count);
+			count += ft_putchar_pf(format, count);
+		format++;
 	}
+	va_end(args);
 	return (count);
+}
+
+int main()
+{
+	ft_printf("Hello baby");
+	return (0);
 }
